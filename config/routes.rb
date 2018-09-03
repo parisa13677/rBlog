990521a00de38d55e24e08ec1a378086f95260e1
parisa <parisa.golanbary@gmail.com>
2018-09-03 15:36:31 +0430

    create admin/sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+  end
+  namespace :admin do
     get 'users/index'
     get 'users/show'
     get 'users/new'
