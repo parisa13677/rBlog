6ef627ec4189632849fb4813f1a62a062af29202
parisa <parisa.golanbary@gmail.com>
2018-09-03 15:31:17 +0430

    create admin/users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/new'
+    get 'users/edit'
+    get 'users/create'
+    get 'users/update'
+    get 'users/destroy'
+  end
+  namespace :admin do
     get 'comments/destroy'
   end
   get 'comment/new'
