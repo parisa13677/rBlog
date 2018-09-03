5824524ed6f352092b2a686cb2034144d44738d9
parisa <parisa.golanbary@gmail.com>
2018-09-03 16:34:18 +0430

    create comment model and shema with rails command.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_115757) do
+ActiveRecord::Schema.define(version: 2018_09_03_120346) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
