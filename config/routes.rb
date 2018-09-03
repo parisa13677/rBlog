dbca6124dc662c2016712760dce0d6f40f67a686
parisa <parisa.golanbary@gmail.com>
2018-09-03 15:02:20 +0430

    create categoriers controller with this command:rails g controller categories then creat all file for it such as javascripts,stlesheets,and other file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'categories/show'
+  namespace :admin do
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/destroy'
+    get 'posts/index'
+    get 'posts/show'
+  end
   get 'posts/index'
   get 'posts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
