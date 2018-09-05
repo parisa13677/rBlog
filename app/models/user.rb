748517629d6cea8ead20b369a0e16520b0b5874c
parisa <parisa.golanbary@gmail.com>
2018-09-05 19:34:50 +0430

    add code validate and variable to post model for connect with other model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# class model user
 class User < ApplicationRecord
   has_many :posts
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: true
 end
