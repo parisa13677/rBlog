dda84e256810a7505796674542636e51becff405
parisa <parisa.golanbary@gmail.com>
2018-09-06 22:16:23 +0430

    add method search and some variables.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :category
-  belongs_to :users
+  belongs_to :user
   has_many :comments
 
   validates :title, presence: true
   validates :category_id, presence: true
   validates :body, presence: true
+
+  has_attached_file :image, :default_url => ":style/rails1.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+
+  def self.search(query)
+    where('title like ? or body like ?', "%#{query}%", "%#{query}%")
+  end
 end
