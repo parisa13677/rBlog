8382a25a6e93ae3e914b6553edc9286556eb40cb
parisa <parisa.golanbary@gmail.com>
2018-09-07 01:42:20 +0430

    add method create and comment_params .

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
-class CommentController < ApplicationController
-  def new
+# frozen_string_literal: true
+
+# class commentscontroller
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    flash[:notice] = 'Comment Added'
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :email, :body, :post_id)
   end
 end
