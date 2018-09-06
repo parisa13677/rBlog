a0350546e95c4abc07c9358113f39ed6542fe26a
parisa <parisa.golanbary@gmail.com>
2018-09-07 02:59:57 +0430

    add method create and destroy.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,23 @@
 
 # this is a sessions controller
 class Admin::SessionsController < Admin::ApplicationController
+  layout 'admin/login'
   def new; end
 
-  def create; end
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to admin_posts_path, :notice => 'Logged In'
+    else
+      flash.now.alert = 'Invalid email password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to admin_login_path, :notice => 'Logged Out'
+  end
 end
+
