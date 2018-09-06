f0729bcfe4fe67b9a90141957aa6859ef6ac98db
parisa <parisa.golanbary@gmail.com>
2018-09-06 21:42:15 +0430

    add condition if to search(def index),for $ if image(def update).

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,7 +8,11 @@ class Admin::PostsController < Admin::ApplicationController
   end
 
   def create
-    @post = Category.new(category_params)
+    @post = Post.new(post_params)
+
+    if params[:post][:image].blank?
+      @post.image = nil
+    end
     if @post.save
       flash[:notice] = 'Post Created'
       redirect_to admin_posts_path
@@ -23,6 +27,10 @@ class Admin::PostsController < Admin::ApplicationController
 
   def update
     @post = Post.find(params[:id])
+
+    if params[:post][:image].blank?
+      @post.image = nil
+    end
 
     if @post.update(post_params)
       flash[:notice] = 'Post Updated'
@@ -39,15 +47,18 @@ class Admin::PostsController < Admin::ApplicationController
 
     flash[:notice] = 'Post Removed'
 
-      redirect_to admin_posts_path
+    redirect_to admin_posts_path
   end
 
   def index
-    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search]).all.order('created_at DESC').paginate(:per_page => 2, :page => params[:page])
+    else
+      @posts = Post.all.order('created_at DESC').paginate(:per_page => 2, :page => params[:page])
+    end
   end
 
-  def show
-  end
+  def show; end
 
 private
 
